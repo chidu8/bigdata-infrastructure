55e767627d3a0fbbfebad90d438e6f5c9e49b786
Chidu <chidu8@users.noreply.github.com>
2014-12-01 23:53:15 -0800

    first commit! First iteration of program to convert a string to its binary hash using SHA algorithm

---
+++ Hyperloglog/src/common/ConvertToBinaryHash.java
@@ -0,0 +1,37 @@
+package common;
+
+import java.math.BigInteger;
+
+public class ConvertToBinaryHash {
+
+
+	
+	public static String encrypt(String stringText) {
+		String sha256hex = org.apache.commons.codec.digest.DigestUtils.sha256Hex(stringText);  
+		return sha256hex;
+	}
+	
+	static String hexToBin(String s) {
+		  return new BigInteger(s, 16).toString(2);
+		}
+	
+	public static String toBinaryHash(String number) {
+		
+		String binsha ="";
+		try {
+			String sha = encrypt(number);
+			binsha = hexToBin(sha);
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return binsha;
+	}
+	public static void main(String[] args) {
+		System.out.println(toBinaryHash("Strings"));
+
+	}
+
+}
